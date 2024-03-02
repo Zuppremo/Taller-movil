3668e95fd2537f4ac587487ac490b5d1acd75741
jjperez26 <jjperez26@misena.edu.co>
2024-03-01 19:47:49 -0500

    Added form values to form

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit
+{
+  formLogin = new FormGroup({
+    userName: new FormControl,
+    password: new FormControl
+  });
+
+  ngOnInit(): void 
+  {
+
+  }
+
+  OnLogin(form:any){
+    console.log(form);
+  }
 }
